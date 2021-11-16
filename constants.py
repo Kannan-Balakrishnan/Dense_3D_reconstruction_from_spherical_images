cea18efdfbd4db0f497dae7153b393873a514b6f
Kannan-Balakrishnan <balakris@rhrk.uni-kl.de>
2021-11-16 10:51:57 +0100

    separate file for constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+import math
+class Constants:
+
+    def __init__(self) -> None:
+        self.pi=math.pi
+        self.pi2=self.pi*2
+        self.KPiInverted = 1/self.pi
+        self.KPi2Inverted= 0.5*self.KPiInverted
--- coordinateconversion.py
+++ coordinateconversion.py
@@ -1,19 +1,16 @@
 import numpy as np
-import math
+from constants import Constants
 class CoordinateConversions:
 
     def __init__(self) -> None:
-        self.pi=math.pi
-        self.pi2=self.pi*2
-        self.KPiInverted = 1/self.pi
-        self.KPi2Inverted= 0.5*self.KPiInverted
+        self.constant=Constants()
 
     def sphereMapCoordsToSpherical(self,x,y,imgWidth,imgHeight):
         # Compute spherical theta coordinate
-        theta = (1. - (x + .5) / imgWidth) * self.pi2
+        theta = (1. - (x + .5) / imgWidth) * self.constant.pi2
         # Now theta is in [0, 2Pi]
         # Compute spherical phi coordinate
-        phi = ((y + .5) * self.pi) / imgHeight
+        phi = ((y + .5) * self.constant.pi) / imgHeight
         #Now phi is in [0, Pi]
         return (phi, theta)
 
@@ -25,8 +22,8 @@ class CoordinateConversions:
         return self.sphericalToCartesian(phi, theta, 1)
 
     def sphericalToSphereMapCoords(self, phi,theta, imgWidth, imgHeight):
-        x=imgWidth * (1. - (theta * self.KPi2Inverted)) - .5
-        y=(imgHeight * phi) * self.KPiInverted - .5
+        x=imgWidth * (1. - (theta * self.constant.KPi2Inverted)) - .5
+        y=(imgHeight * phi) * self.constant.KPiInverted - .5
         return x,y
 
     def unitCartesianToSphereMapCoords(self, vec, imgWidth,imgHeight):
@@ -37,12 +34,12 @@ class CoordinateConversions:
         if abs(vec[2])==1:
             KTheta=0
         else:
-            KTheta=np.arctan2(vec[1], vec[0]) * self.KPi2Inverted
+            KTheta=np.arctan2(vec[1], vec[0]) * self.constant.KPi2Inverted
         if (KTheta < 0):
             x = -KTheta * imgWidth - .5;           # -0.5 < x <= imgWidth/2 - 0.5 (left half of the pixel map)
         else:
             x = (1. - KTheta) * imgWidth - .5;     #imgWidth/2 - 0.5 <= x <= imgWidth - 0.5 (right half of the pixel map)
-        KPhi=min(np.arccos(vec[2]) * self.KPiInverted,1)
+        KPhi=min(np.arccos(vec[2]) * self.constant.KPiInverted,1)
         y = KPhi * imgHeight - .5
         return x,y
 
